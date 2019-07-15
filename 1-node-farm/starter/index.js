e659015c6838def00d44ad2d33ad0e4d53ff08af
Andrew McMechan <coderwurst@gmail.com>
2019-07-15 21:18:57 +0200

    read and write using file system module

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,2 +1,8 @@
-const hello = 'hello world';
-console.log(hello);
+const fs = require('fs');
+
+const avacato = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(avacato);
+
+const textOutput = `${avacato} Avocado is also a popular choice for smoothies!\nCreated on ${Date.now()}`
+fs.writeFileSync('./txt/textOutput.txt', textOutput);
+console.log('file written!');
