869237d2be5dfa9d1ea6fac96c26a0ecb6c3c017
Andrew McMechan <coderwurst@gmail.com>
2019-07-28 07:43:05 +0200

    retrieve the retriever from dog.ceo

---
+++ async-js/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const superagent = require('superagent')
+
+fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
+    if (error) return console.log(error.message);
+    console.log('Breed: ' + data);
+    superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((error, response) => {
+        if(error) return console.log(error.message);
+        console.log(response.body.message);
+        fs.writeFile('dog-img.txt', response.body.message, (error) => {
+            if (error) return console.log(error.message);
+            console.log('image saved to file');
+        });
+    });
+});
