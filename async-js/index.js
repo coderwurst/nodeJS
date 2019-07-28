619f690e787ba95ffef802729ea1fec5ca1ea0fc
Andrew McMechan <coderwurst@gmail.com>
2019-07-28 07:56:27 +0200

    create own promise for fs read

--- async-js/index.js
+++ async-js/index.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const superagent = require('superagent')
 
-fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
-    if (error) return console.log(error.message);
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (error, data) => {
+            if (error) reject(error.message);       // available later in the catch method
+            resolve(data);                          // available later in then method
+        });
+    });
+}
+
+readFilePro(`${__dirname}/dog.txt`).then( data => {
     console.log('Breed: ' + data);
     superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
