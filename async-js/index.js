50a635e195f9127d1418438cfb5fb22ed08aa6f5
Andrew McMechan <coderwurst@gmail.com>
2019-07-28 07:50:48 +0200

    consume promise from superagent lib

--- async-js/index.js
+++ async-js/index.js
@@ -6,12 +6,13 @@ fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
     console.log('Breed: ' + data);
     superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((error, response) => {
-        if(error) return console.log(error.message);
+    .then(response => {
         console.log(response.body.message);
         fs.writeFile('dog-img.txt', response.body.message, (error) => {
             if (error) return console.log(error.message);
             console.log('image saved to file');
         });
+    }).catch(error => {
+        if (error) return console.log(error.message);
     });
 });
